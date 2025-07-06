94869bb72a3fd4775cd6508e5a18714809ca0cda
kkcoder121 <flymax121@gmail.com>
2025-07-06 23:39:09 +0530

    Add files via upload
    fixed errors

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 function generateVoucher() {
-    document.getElementById('vGuestName').textContent = document.getElementById('guestName').value;
-    document.getElementById('vBookingId').textContent = document.getElementById('bookingId').value;
-    document.getElementById('vBookingRef').textContent = document.getElementById('bookingRef').value;
-    document.getElementById('vHotelName').textContent = document.getElementById('hotelName').value;
-    document.getElementById('vAddress').textContent = document.getElementById('address').value;
-    document.getElementById('vCheckIn').textContent = document.getElementById('checkIn').value;
-    document.getElementById('vCheckOut').textContent = document.getElementById('checkOut').value;
-    document.getElementById('vRooms').textContent = document.getElementById('rooms').value;
-    document.getElementById('vAdults').textContent = document.getElementById('adults').value;
-    document.getElementById('vChildren').textContent = document.getElementById('children').value;
-    document.getElementById('vRoomType').textContent = document.getElementById('roomType').value;
-    document.getElementById('vBenefits').textContent = document.getElementById('benefits').value;
+  document.getElementById('vGuestName').textContent = document.getElementById('guestName').value;
+  document.getElementById('vBookingId').textContent = document.getElementById('bookingId').value;
+  document.getElementById('vReferenceNo').textContent = document.getElementById('referenceNo').value;
+  document.getElementById('vHotelName').textContent = document.getElementById('hotelName').value;
+  document.getElementById('vAddress').textContent = document.getElementById('address').value;
+  document.getElementById('vCheckinDate').textContent = document.getElementById('checkinDate').value;
+  document.getElementById('vCheckoutDate').textContent = document.getElementById('checkoutDate').value;
+  document.getElementById('vRooms').textContent = document.getElementById('rooms').value;
+  document.getElementById('vAdults').textContent = document.getElementById('adults').value;
+  document.getElementById('vChildren').textContent = document.getElementById('children').value;
+  document.getElementById('vRoomType').textContent = document.getElementById('roomType').value;
+  document.getElementById('vBenefits').textContent = document.getElementById('benefits').value;
 }
